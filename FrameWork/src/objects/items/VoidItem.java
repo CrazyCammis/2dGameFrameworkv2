29a813144bb918da02b370352f64f45e644fe97d
Levi <leventen@hiof.no>
2021-03-09 16:24:58 +0100

    Fixed error in VoidItem

--- FrameWork/src/objects/items/VoidItem.java
+++ FrameWork/src/objects/items/VoidItem.java
@@ -1,7 +1,7 @@
 package objects.items;
 
 public class VoidItem extends Item {
-    public VoidItem(int posX, int posY) {
-        super(posX, posY);
+    public VoidItem(int posX, int posY, String imageFile) {
+        super(posX, posY, imageFile);
     }
 }
